3f29bc424428ab153378d00bcf95037832def131
Pedro França <pfranca.neto@gmail.com>
2022-06-22 19:21:53 -0300

    criando aside | map | entre outros

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,5 +1,38 @@
-export function Lesson() {
-  return(
-    <div>Lesson</div>
-  )
+import { CheckCircle, Lock } from "phosphor-react";
+interface LessonProps {
+  title: string;
+  slug: string;
+  availableAt: Date;
+  type: "live" | "class";
+}
+
+export function Lesson(props: LessonProps) {
+  const isLessonAvailable = false;
+
+  return (
+    <div>
+      <a href="#">
+        <span className="text-gray-300">{props.availableAt.toString()}</span>
+        <div className="rounded border border-gray-500 p-4 mt-2">
+          <header className="flex items-center justify-between">
+            {isLessonAvailable ? (
+              <span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
+                <CheckCircle size={20} />
+                Conteúdo Liberado
+              </span>
+            ) : (
+              <span className="flex items-center gap-2 text-sm text-orange-500 font-medium">
+                <Lock size={20} />
+                Em Breve
+              </span>
+            )}
+            <span className="text-xs rounded border py-[0.125rem] px-2 text-white border-green-300 font-bold">
+              {props.type === "live" ? "AO VIVO" : "AULA PRÁTICA"}
+            </span>
+          </header>
+          <strong className="text-gray-200 mt-5 block">{props.title}</strong>
+        </div>
+      </a>
+    </div>
+  );
 }
--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -5,8 +5,12 @@ import { Video } from "../components/Video";
 
 export function Event() {
   return (
-    <div>
+    <div className="flex flex-col">
       <Header />
+      <main className="flex flex-1 min-h-screen">
+        <Video />
+        <Sidebar />
+      </main>
     </div>
   )
 }
