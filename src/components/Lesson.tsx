c034d8a3c9c59e19615235f992ea16adf26e776a
Pedro França <pfranca.neto@gmail.com>
2022-06-23 10:33:36 -0300

    Ajustando componente Lesson

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,4 +1,6 @@
 import { CheckCircle, Lock } from "phosphor-react";
+import { isPast, format } from 'date-fns';
+import ptBr from 'date-fns/locale/pt-BR';
 interface LessonProps {
   title: string;
   slug: string;
@@ -7,12 +9,17 @@ interface LessonProps {
 }
 
 export function Lesson(props: LessonProps) {
-  const isLessonAvailable = false;
+  const isLessonAvailable = isPast(props.availableAt);
+  const availableDateFormatted = format(props.availableAt, "EEEE ' • 'd' de 'MMMM' • 'K'h'mm", {
+    locale: ptBr,
+  })
 
   return (
     <div>
       <a href="#">
-        <span className="text-gray-300">{props.availableAt.toString()}</span>
+        <span className="text-gray-300">
+          {availableDateFormatted}
+        </span>
         <div className="rounded border border-gray-500 p-4 mt-2">
           <header className="flex items-center justify-between">
             {isLessonAvailable ? (
