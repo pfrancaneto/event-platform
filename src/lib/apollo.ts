4f947573ccb731241d63615f4a0cdcd56a8a9901
Pedro França <pfranca.neto@gmail.com>
2022-06-23 21:38:32 -0300

    conectando componentes ao graphQL

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,10 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4nzw15v07l401z35qdbe68i/master',
+  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4rl8pw416mr01yx6j87fjyo/master',
   cache: new InMemoryCache()
 })
+
+
+
+// https://api-sa-east-1.graphcms.com/v2/cl4nzw15v07l401z35qdbe68i/master - funciona
