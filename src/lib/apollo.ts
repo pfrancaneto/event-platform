01c39493c0086d89548e29e75d7a400d3e0151e6
Pedro França <pfranca.neto@gmail.com>
2022-06-28 08:43:26 -0300

    criando variáveis ambiente

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,10 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4rl8pw416mr01yx6j87fjyo/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
-
-
-
-// https://api-sa-east-1.graphcms.com/v2/cl4nzw15v07l401z35qdbe68i/master - funciona
